99d76e5789c84a47896fb9d15bda82515308cb1d
j03-dev <24nomeniavo@gmail.com>
2024-04-26 21:06:50 +0300

    Update project titles and links in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -95,14 +95,14 @@ export class AppComponent {
   ];
   projects = [
     {
-      title: 'tish',
+      title: 'Tish',
       image: 'project_2.png',
       description:
         'In the dynamic realm of advanced web development, I embarked on a project that showcases my expertise in Java Enterprise Edition (JEE) with JavaServer Pages (JSP) and Servlet technologies. The project revolves around the creation of a fully functional e-commerce website dedicated to the vibrant world of fashion.',
       link_project: 'https://github.com/tbgracy/tish',
     },
     {
-      title: 'slate',
+      title: 'Slate',
       image: 'project_3.png',
       description:
         'In the spirit of fostering collaborative learning and innovation, our team participated in a hackathon organized by IFM (Institut Français de Madagascar) with a focus on education. The result of our collaborative efforts is Slate, a web application designed to facilitate knowledge-sharing through a dynamic question-and-answer platform, influenced by the collaborative spirit of Full Stack Overflow.',
@@ -113,7 +113,7 @@ export class AppComponent {
       image: 'project_4.png',
       description:
         "Embarking on a journey to share my insights, experiences, and technological adventures, I've created Joe's blog, a dynamic blog platform powered by Django's robust template rendering engine. This blog serves as a digital canvas where I articulate my thoughts, discoveries, and learnings within the vast realm of technology.",
-      link_project: 'https://nomeniavojoe.pythonanywhere.com/',
+      link_project:'https://nomeniavo-joe-blog.onrender.com/',
     },
   ];
 }
